b647b1ce561ee2d3ac15c540a3d106fa78f52856
aisha1498 <95531968+aisha1498@users.noreply.github.com>
2022-03-01 11:29:46 +0530

    Add files via upload

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -16,3 +16,14 @@ Email varchar(60)
 )
 
 select * from Addressbook
+
+--UC3 inserting of new contacts
+
+insert into Addressbook values
+('Aishwarya','Ingle','Maharashtra','Aurangabad','4321','1596321478','aisha@gmail.com'),
+('Twisha','Singh','Telangana','Hyderabad','6543','8697412456','twisha@gmail.com'),
+('Shaurya','Patil','Pune','Maharashtra','8765','4563217895','shaurya@gmail.com'),
+('Siya','Waghchaure','Nashik','Maharashtra','9876','7531598625','siya@gmail.com'),
+('Arnavi','Ghanghav','Mumbai','Maharashtra','3214','785123654','arnavi@gmail.com')
+
+select * from Addressbook
